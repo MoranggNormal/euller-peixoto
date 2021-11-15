12bb1938e9b236ac80f0c02bffdb0fdb73821a04
Euller Peixoto <peixotoeuller500@gmail.com>
2021-11-15 18:16:49 -0500

    main image border now radomly changes

---
+++ js/randomBorder.js
@@ -0,0 +1,22 @@
+const img = document.querySelector(".main-pic");
+
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+setInterval(() => {
+  img.style.borderRadius = `${randomIntFromInterval(
+    30,
+    80
+  )}% ${randomIntFromInterval(20, 100)}% ${randomIntFromInterval(
+    30,
+    80
+  )}% ${randomIntFromInterval(20, 100)}% / ${randomIntFromInterval(
+    30,
+    80
+  )}% ${randomIntFromInterval(20, 100)}% ${randomIntFromInterval(
+    30,
+    80
+  )}% ${randomIntFromInterval(20, 100)}%`;
+}, 2500);
