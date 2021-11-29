1f1cee5a261a94036a590f46623f1e57e87a7458
Euller Peixoto <peixotoeuller500@gmail.com>
2021-11-29 20:25:09 -0300

    setup contacts page and send email via EmailJS

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,40 @@
+import { init, send } from "emailjs-com";
+init("user_jNBh3414Iz9HJlwjPGryn");
+
+const form = document.querySelector(".contact-form");
+const recruiterEmail = document.querySelector(".recruiter-email");
+const recruiterMessage = document.querySelector(".recruiter-message");
+const setError = document.querySelector(".contact-message-error");
+const setSucess = document.querySelector(".contact-message-sucess");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (recruiterEmail.value.length != 0 && recruiterMessage.value.length != 0) {
+
+    send("service_r10u1x6", "template_tbvsjp6", {
+      from_name: recruiterEmail.value,
+      to_name: "Euller",
+      message: recruiterMessage.value,
+      reply_to: "euller",
+    });
+
+    setSucess.style.display = "block";
+
+    recruiterEmail.value = "";
+    recruiterMessage.value = "";
+    setError.style.display = "none";
+
+    setTimeout(() => {
+      setSucess.style.display = "none";
+    }, 2000);
+
+    return
+
+  } 
+
+  setError.style.display = "block";
+
+
+
+});
