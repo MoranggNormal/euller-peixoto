f8bcdb792ab6d818c3c7e57e75212c312b0ae322
Euller Peixoto <peixotoeuller500@gmail.com>
2021-12-18 03:34:02 -0300

    code reformat

--- assets/js/cursorEffect.js
+++ assets/js/cursorEffect.js
@@ -1,30 +1,27 @@
 const mainCursor = document.querySelector(".cursor-main");
 const followCursor = document.querySelector(".cursor-follow");
 
-
 document.addEventListener("mousemove", (e) => {
-  let x = e.clientX;
-  let y = e.clientY;
+    let x = e.clientX;
+    let y = e.clientY;
 
-  followCursor.style.top = y + "px";
-  followCursor.style.left = x + "px";
+    followCursor.style.top = y + "px";
+    followCursor.style.left = x + "px";
 
-  mainCursor.style.top = y + "px";
-  mainCursor.style.left = x + "px";
+    mainCursor.style.top = y + "px";
+    mainCursor.style.left = x + "px";
 
-  if (e.target.className.includes('hoverable')) {
-      mainCursor.style.width = '4.9em'
-      mainCursor.style.height = '4.9em'
+    if (e.target.className.includes("hoverable")) {
+        mainCursor.style.width = "4.9em";
+        mainCursor.style.height = "4.9em";
 
-      followCursor.style.width = '1em'
-      followCursor.style.height = '1em'
+        followCursor.style.width = "1em";
+        followCursor.style.height = "1em";
+    } else {
+        mainCursor.style.width = "2.9em";
+        mainCursor.style.height = "2.9em";
 
-  }else{
-      mainCursor.style.width = '2.9em'
-      mainCursor.style.height = '2.9em'
-
-      followCursor.style.width = '0.5em'
-      followCursor.style.height = '0.5em'
-  }
-
+        followCursor.style.width = "0.5em";
+        followCursor.style.height = "0.5em";
+    }
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,32 @@
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll("nav .container ul li");
 
-
 window.onscroll = () => {
-  let current = "";
+    let current = "";
 
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop;
-    if (scrollY >= sectionTop - 60) {
-      current = section.getAttribute("id");
-    }
-  });
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop - 60) {
+            current = section.getAttribute("id");
+        }
+    });
 
-  navLi.forEach((li) => {
-    li.classList.remove("active");
-    if (li.classList.contains(current)) {
-      li.classList.add("active");
-    }
-  });
+    navLi.forEach((li) => {
+        li.classList.remove("active");
+        if (li.classList.contains(current)) {
+            li.classList.add("active");
+        }
+    });
 };
 
-const menu = document.querySelector('.Menu')
-const toggleMenu = document.querySelector('.toggle-menu')
+const menu = document.querySelector(".Menu");
+const toggleMenu = document.querySelector(".toggle-menu");
 
-toggleMenu.addEventListener('click', () => {
-  toggleMenu.classList.toggle('menu-open')
-  menu.classList.toggle('show-menu')
+toggleMenu.addEventListener("click", () => {
+    toggleMenu.classList.toggle("menu-open");
+    menu.classList.toggle("show-menu");
 
-  sections.forEach((e) => {
-    e.classList.toggle('blur')
-  })
-})
+    sections.forEach((e) => {
+        e.classList.toggle("blur");
+    });
+});
